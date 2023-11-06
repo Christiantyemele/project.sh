1eb40490ab8a6b21dd32bde3d8ac389d445afe75
christiantyemele <yemelechristian2@gmail.com>
2023-11-06 12:11:30 +0100

    final commit

--- init_project.sh
+++ init_project.sh
@@ -1,21 +1,60 @@
-interface() {
+func() {
+  interface() {
 	echo "please choose your project type"
-	echo "1. python project
-	      2. bash project"
-      }
+	echo "1. python project"
+	echo "2. bash project"
+    echo "3. exit"      
+}
+
       interface
+        
 
-	      read project_type
+      
+	read project_type
+        case $project_type in
 
-        if $project_type == 1:
-		echo "please type in the project's name"
-	 	 read project_name
-                  python3 -m venv $project_name
-		interface
-
-        elif $project_type == 2:
-		echo "please type in project's name"
-		read project_name
-		touch README > /dev/null
+		1)
+	        echo ""
+	       	echo "please type in the project's name"
+	 	read project_name
+                python3 -m venv $project_name
+		echo "your virtual environment has been successfully created"
+		 
 	
+                 interface
+                 func;;
+                 
+	        2) 
+	
+                echo ""
+	       	echo "please type in project's name"
+		read project_name
+		echo ""
+		echo "a README file has been created showing how you should proceed"
+	
+		touch README > /dev/null
+	        
+               
+               interface
+               func;;
+        
+                3) 
 
+	     	
+	      
+		 exit;;               
+           	
+                *)
+	         echo ""
+   		 echo "please enter a valid option"
+		 echo ""
+		 
+		 func;;
+		 
+
+               
+               	 
+      
+       esac
+}
+func
