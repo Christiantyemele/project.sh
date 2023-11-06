689c88495eca82cfdd4404c197dce3358fc32ea1
christiantyemele <yemelechristian2@gmail.com>
2023-11-06 10:32:57 +0100

    second commit

--- init_project.sh
+++ init_project.sh
@@ -4,10 +4,18 @@ interface() {
 	      2. bash project"
       }
       interface
+
 	      read project_type
+
         if $project_type == 1:
 		echo "please type in the project's name"
 	 	 read project_name
                   python3 -m venv $project_name
 		interface
+
+        elif $project_type == 2:
+		echo "please type in project's name"
+		read project_name
+		touch README > /dev/null
 	
+
